583d1a97431eeb0ec28c72ce4525681b377d5124
GARCE Leila <luma.store.patagonia@gmail.com>
2022-11-09 19:25:41 -0300

    Java Programacion

---
+++ JavaProgramacion/src/javaprogramacion/JavaProgramacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaprogramacion;
+
+/**
+ *
+ * @author Julieta
+ */
+public class JavaProgramacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
